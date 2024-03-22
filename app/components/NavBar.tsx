f8859165662470601f1c45ea9e820fed26e58eba
nhussein11 <nicolashussein14@gmail.com>
2024-03-22 15:05:33 -0300

    add: initial commit

--- app/components/NavBar.tsx
+++ app/components/NavBar.tsx
@@ -2,16 +2,20 @@ export const NavBar = () => {
   return (
     <div className="flex justify-between py-4 px-12 text-xl">
       <div className="flex justify-start items-center text-3xl cursor-pointer">
-        <span className="text-white">Traceable Chain</span>
         <img  
           src="/treceability-logo.png"
           alt="traceable chain logo"
           className="text-white invert h-16 w-16 mx-4"
         />
+
+        <span className="text-white">Traceable Chain</span>
       </div>
       <div className="flex justify-end gap-x-8">
         <div className="hover:underline underline-offset-8	">
           <a href="/">Whitepaper</a>
+        </div>
+        <div className="hover:underline underline-offset-8	">
+          <a href="/solutions">Solutions</a>
         </div>
         <div className="hover:underline underline-offset-8	">
           <a href="/about">About</a>
--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,22 @@
-
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      test
+    <main className="flex min-h-screen flex-col items-center p-24">
+      <div className="flex flex-row justify-between items-center h-full w-full mt-24 px-16">
+        <div className="flex flex-col gap-4">
+          <h1 className="text-5xl font-bold">Transforming Traceability with Blockchain</h1>
+          <p className="text-[1.65rem] mt-2">
+            Empowering your business with unparalleled transparency and security
+          </p>
+          <button className="w-fit bg-gradient-to-r from-blue-700 to-blue-900 hover:opacity-[0.7] text-lg text-white  mt-8 py-2 px-8 rounded-lg">
+            Learn More
+          </button>
+        </div>
+        <img 
+          src="/blockchain-intro.webp"
+          alt="Blockchain Introduction"
+          className="h-[27rem] w-[32rem] ml-4 opacity-[0.4]"
+        />
+      </div>
     </main>
   );
 }

132a59261f809bffb486ddf5b48f3ab915d0dfc7
nhussein11 <nicolashussein14@gmail.com>
2024-05-12 19:21:25 -0300

    fix: updating contact us page

--- app/contact/page.tsx
+++ app/contact/page.tsx
@@ -1,7 +1,15 @@
 export default function Contact(){
   return (
-    <div className="flex flex-col items-center gap-8">
-      <h1 className="text-5xl font-bold">Contact</h1>
+    <div className="flex flex-col items-start m-20">
+      <h1 className="text-4xl font-bold">Feel free to keep in touch</h1>
+      <form className="flex flex-col gap-4 mt-8">
+        <input className="w-96 p-2 rounded-lg" type="text" placeholder="Name" />
+        <input className="w-96 p-2 rounded-lg" type="email" placeholder="Email" />
+        <textarea className="w-96 h-32 p-2 rounded-lg" placeholder="Message"></textarea>
+        <button className="w-96 bg-gradient-to-r from-blue-700 to-blue-900 hover:opacity-[0.7] text-lg text-white py-2 px-8 rounded-lg">
+          Send
+        </button>
+      </form>
     </div>
   );
 }
--- app/solutions/page.tsx
+++ app/solutions/page.tsx
@@ -1,22 +1,4 @@
-type SolutionCardProps = {
-  title: string;
-  description: string;
-  buttonText: string;
-}
-
-const SolutionCard = ({title, description, buttonText}: SolutionCardProps) => {
-  return (
-    <div className="flex flex-col gap-4 p-8 bg-gray-300 bg-opacity-25 rounded-lg">
-      <h2 className="text-3xl font-bold">{title}</h2>
-      <p className="text-lg h-32">
-        {description}
-      </p>
-      <button className="w-fit bg-gradient-to-r from-blue-700 to-blue-900 hover:opacity-[0.7] text-lg text-white  mt-8 py-2 px-8 rounded-lg">
-        {buttonText}
-      </button>
-    </div>
-  );
-}
+import { SolutionCard } from "../components/Solutions/SolutionCard";
 
 const SOLUTIONS = [
   {
